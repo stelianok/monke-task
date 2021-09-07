e708e5359b8b838457079e91affa5ccc125e16f7
Kaua Steliano <stelianok@gmail.com>
2021-09-07 03:07:02 -0300

    tasks are now accessible from separate file.

---
+++ src/tasks.ts
@@ -0,0 +1,20 @@
+import { Task } from "./interfaces/Itasks";
+import { getTodoistTasks } from "./todoistAPI";
+
+
+var todoistTasks: Task[];
+
+function startTasks() {
+  getTodoistTasks().then((response) => {
+    todoistTasks = response;
+  }).catch((err) => {
+    console.log(err);
+    return [];
+  });
+}
+
+function setTasks(newTaskArray: Task[]) {
+  todoistTasks = newTaskArray;
+}
+
+export { todoistTasks, startTasks, setTasks };
