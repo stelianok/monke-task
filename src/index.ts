7cae4beb7c78d50ed6bd60c9762efc8f960b80e9
Kaua Steliano <stelianok@gmail.com>
2021-09-07 06:00:02 -0300

    Added listne

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import { notifyNewTasks } from './commands/notifyNewTask';
 
 import { startTasks } from './tasks';
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -43,3 +45,4 @@ client.on('interactionCreate', async (interaction: any) => {
 });
 
 client.login(DISCORD_TOKEN);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
