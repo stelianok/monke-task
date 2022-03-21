2204306f04a56e8f52cff332d4fa013ba9878ad7
Kauã Steliano <stelianok@gmail.com>
2022-03-21 08:53:15 -0300

    slash commands are now registered globally

--- src/deploy-command.js
+++ src/deploy-command.js
@@ -17,7 +17,7 @@ const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
 (async () => {
   try {
     await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, guildId),
+      Routes.applicationCommands(CLIENT_ID),
       { body: commands },
     );
 
