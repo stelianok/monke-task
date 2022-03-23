8e53bc5f414f91df4326a41f6be4be4ed1b33ee2
Kauã Steliano <stelianok@gmail.com>
2022-03-23 11:10:06 -0300

    separated condition on if for better messages

--- src/DiscordMessaging/SendDiscordMessage.ts
+++ src/DiscordMessaging/SendDiscordMessage.ts
@@ -1,8 +1,10 @@
 export default async function SendDiscordMessages(interaction: any, messageArray: string[]): Promise<void> {
-  if(messageArray.length < 1 || !messageArray){
+  if(messageArray.length < 1){
     await interaction.reply("Sem Tarefas! 🥳");
   }
-
+  else if(!messageArray){
+    await interaction.reply("Oops! Parece que um erro ocorreu! checa com o adm 😬");
+  }
 
   if(messageArray.length === 1){
     await interaction.reply(messageArray[0]);
