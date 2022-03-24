fff5293c68506684e2a15b5306b573c27f23ce27
Kauã Steliano <stelianok@gmail.com>
2022-03-24 09:20:58 -0300

    Added error handling,  created new function

--- src/DiscordMessaging/SendDiscordMessage.ts
+++ src/DiscordMessaging/SendDiscordMessage.ts
@@ -1,22 +1,33 @@
-export default async function SendDiscordMessages(interaction: any, messageArray: string[]): Promise<void> {
-  if(messageArray.length < 1){
-    await interaction.reply("Sem Tarefas! 🥳");
-  }
-  else if(!messageArray){
-    await interaction.reply("Oops! Parece que um erro ocorreu! checa com o adm 😬");
-  }
+import { CommandInteraction, Interaction } from "discord.js";
 
-  if(messageArray.length === 1){
-    await interaction.reply(messageArray[0]);
-  }
-  else {
-    for(let i = 0; i <= messageArray.length; i++){
-      if(i <= 0){
-        await interaction.reply(messageArray[0]);
-      }
-      else {
-        await interaction.followUp(messageArray[i]);
-      }
+async function replyToMessages(interaction: CommandInteraction, messageArray: string[]){
+  for(let i = 0; i <= messageArray.length; i++){
+    if(i <= 0){
+      await interaction.reply(messageArray[0]);
     }
+    else {
+      await interaction.followUp(messageArray[i]);
+    }
+  }
+}
+
+export default async function SendDiscordMessages(interaction: CommandInteraction, messageArray: string[]): Promise<void> {
+  try {
+    if(messageArray.length < 1){
+      await interaction.reply("Sem Tarefas! 🥳");
+    }
+    else if(!messageArray){
+      await interaction.reply("Oops! Parece que um erro ocorreu! checa com o adm 😬");
+    }
+  
+    if(messageArray.length === 1){
+      await interaction.reply(messageArray[0]);
+    }
+    else {
+      await replyToMessages(interaction, messageArray);
+    }
+  }
+  catch(err){
+    console.log(err);
   }
 }
