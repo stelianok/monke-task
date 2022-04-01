7132d6c9eee45a0801840a244828f08667a4dc14
Kauã Steliano <stelianok@gmail.com>
2022-04-01 10:32:52 -0300

    fixed typescript bug

--- src/commands/getAllTasks.ts
+++ src/commands/getAllTasks.ts
@@ -1,4 +1,4 @@
-import { Interaction } from "discord.js";
+import { CommandInteraction, Interaction } from "discord.js";
 
 import { createMessagesArray } from "../DiscordMessaging/formatDiscordMessages"
 import SendDiscordMessage from "../DiscordMessaging/SendDiscordMessage";
@@ -6,12 +6,12 @@ import SendDiscordMessage from "../DiscordMessaging/SendDiscordMessage";
 import { Task } from "../interfaces/Itasks";
 import { getTodoistTasks } from "../todoistAPI";
 
-async function GetAllTasks(interaction: Interaction, filter: string) {
+async function GetAllTasks(interaction: CommandInteraction, filter: string) {
   try {
     const tasks: Task[] = await getTodoistTasks(filter, interaction.guild?.id);
 
     const messages = createMessagesArray(tasks);
-    
+
     await SendDiscordMessage(interaction, messages);
   }
   catch (err) {
